21543f8fa4680b4c27341237c2156ddc320f5830
Rishab Vempati <143560701+rvempat@users.noreply.github.com>
2023-10-31 00:24:28 -0400

    Create osm_to_json.py

---
+++ osm_to_json.py
@@ -0,0 +1,32 @@
+import requests
+import json
+
+overpass_url = "http://overpass-api.de/api/interpreter"
+overpass_query = (
+    '[out:json];'
+    '(way["parking"="surface"](39.7676,-86.1666,39.80339,-86.10255);'
+    '  (._;>;);'
+    ');'
+    'out body;'
+)
+
+headers = {
+    'Accept': 'application/json'  
+}
+
+try:
+    response = requests.get(overpass_url, params={'data': overpass_query}, headers=headers)
+    response.raise_for_status()
+
+    data = response.json()
+    output_json = json.dumps(data, indent=4)
+
+    print(output_json)
+
+    with open('data.json', 'w') as f:
+        json.dump(output_json, f)
+
+except requests.exceptions.HTTPError as e:
+    print(f"HTTP Error: {e}")
+except Exception as e:
+    print(f"An error occurred: {e}")
