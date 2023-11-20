7e797b1c14b5011f92246b5f8d35b9e8a82164fc
Siddharth Sancheti <sidsancheti94@gmail.com>
2023-11-19 22:57:36 -0800

    Changed the OverpassTurbo query to get a larger region

--- osm_to_json.py
+++ osm_to_json.py
@@ -1,37 +1,38 @@
 import requests
 import json
 
+# Overpass API URL
 overpass_url = "http://overpass-api.de/api/interpreter"
 
-# Query for all surface parking lots in Indianapolis
-overpass_query = (
-    '[out:json];'
-    '(way["parking"="surface"](39.7676,-86.1666,39.80339,-86.10255);'
-    '  (._;>;);'
-    ');'
-    'out body;'
-)
+# Your Overpass query
+overpass_query = """
+[out:json][timeout:100];
+// gather results
+(
+  // query part for: “parking=surface”
+  node["parking"="surface"](39.7042,-86.3994,39.9569,-86.0172);
+  way["parking"="surface"](39.7042,-86.3994,39.9569,-86.0172);
+  relation["parking"="surface"](39.7042,-86.3994,39.9569,-86.0172);
+);
+// print results
+out body;
+>;
+out skel qt;
+"""
 
-headers = {
-    'Accept': 'application/json'  
-}
+# Send the request
+response = requests.get(overpass_url, 
+                        params={'data': overpass_query})
 
-try:
-    response = requests.get(overpass_url, params={'data': overpass_query}, headers=headers)
-    response.raise_for_status()
+# Check for HTTP errors
+response.raise_for_status()
 
-    data = response.json()
-    output_json = json.dumps(data, indent=4)
+# Parse the JSON response
+data = response.json()
 
-    print(output_json)
+# Print the results or process them as you need
+print(json.dumps(data, indent=4))
 
-    ## json.dump function expects a Python dictionary but this was providing a string before
-    ## corrected code so the json string can be saved directly to the file without calling json.dump
-
-    with open('data.json', 'w') as f:
-        f.write(output_json)
-
-except requests.exceptions.HTTPError as e:
-    print(f"HTTP Error: {e}")
-except Exception as e:
-    print(f"An error occurred: {e}")
+# Save the data to a file
+with open('data.json', 'w') as file:
+    json.dump(data, file, indent=4)
