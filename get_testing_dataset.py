e96af302d0b235accc8a4e7e544ea5333dee2764
Siddharth Sancheti <sidsancheti94@gmail.com>
2023-12-01 17:50:10 -0500

    Modified the save directory of images to external drive; refactored image file name

--- get_testing_dataset.py
+++ get_testing_dataset.py
@@ -1,5 +1,11 @@
 import requests
 import math
+import os
+
+# Ensure the folder exists
+save_dir = 'E:/OSMTestingImages'
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
 
 # Define your bounding box and ground resolution
 min_lat = 39.7416667
@@ -21,30 +27,40 @@ base_url = "https://dev.virtualearth.net/REST/v1/Imagery/Map/Aerial/"
 params = {
     "mapSize": "500,500",
     "mapLayer": "Basemap,Buildings",
-    "key": "Ain7kUv28hvUkTkX5QfhVU-J_rqqtZMk7lGZNjh_e0ivB3wxcJsR3tAHJVAr8ZdC"  # Bing Maps API key
+    "key": "Your_BingMapsKey"  # Replace with your actual Bing Maps API key
 }
 
 # Function to retrieve and save the image
-def get_image(lat, long):
+def get_image(bottom_left_lat, bottom_left_long, top_right_lat, top_right_long):
     # Define the bounding box for the current square
-    map_area = f"{lat},{long},{lat-delta_lat},{long+delta_long}"
+    map_area = f"{bottom_left_lat},{bottom_left_long},{top_right_lat},{top_right_long}"
     # Construct the API URL
     api_url = f"{base_url}{map_area}&mapSize={params['mapSize']}&mapLayer={params['mapLayer']}&key={params['key']}"
     response = requests.get(api_url)
     if response.status_code == 200:
+        # Construct the file path
+        image_filename = os.path.join(save_dir, f"image_{bottom_left_lat}_{bottom_left_long}_{top_right_lat}_{top_right_long}.jpeg")
         # Save the image
-        image_filename = f"image_{lat}_{long}.png"
         with open(image_filename, 'wb') as image_file:
             image_file.write(response.content)
         print(f"Downloaded {image_filename}")
     else:
-        print(f"Failed to download image at {lat}, {long}: {response.status_code}")
+        print(f"Failed to download image at {bottom_left_lat}, {bottom_left_long}: {response.status_code}")
 
 # Iterate over each square within the bounding box
-for i in range(num_lat + 1):
-    for j in range(num_long + 1):
-        # Calculate the latitude and longitude for the top-left corner of the square
-        current_lat = max_lat - i * delta_lat
-        current_long = min_long + j * delta_long
+for lat_step in range(num_lat + 1):
+    for long_step in range(num_long + 1):
+        # Calculate the latitude and longitude for the bottom left corner of the square
+        bottom_left_lat = min_lat + (lat_step * delta_lat)
+        bottom_left_long = min_long + (long_step * delta_long)
+        
+        # Calculate the latitude and longitude for the top right corner of the square
+        top_right_lat = bottom_left_lat + delta_lat
+        top_right_long = bottom_left_long + delta_long
+        
+        # Ensure that we do not exceed the max bounds
+        top_right_lat = min(top_right_lat, max_lat)
+        top_right_long = min(top_right_long, max_long)
+        
         # Retrieve and save the image for the current square
-        get_image(current_lat, current_long)
+        get_image(bottom_left_lat, bottom_left_long, top_right_lat, top_right_long)
